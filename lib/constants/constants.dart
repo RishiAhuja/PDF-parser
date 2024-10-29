7575be6031a3f3a6e44ca23268c1e4003f84101e
Rishi Ahuja <67419764+RishiAhuja@users.noreply.github.com>
2024-10-29 22:28:26 +0530

    API key fixes

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String geminiApiKey = 'AIzaSyCQeD6UY_USqK8T7t3682qEG5wtSjnU2x0';
+  static String geminiApiKey = 'API_KEY';
   static Color backgroundColor = Colors.white;
 
   static String localUsername = "";
